95003ef3a425984995925512fcfcab4342c36957
Thutur39 <antonio.arthur2897@gmail.com>
2020-01-13 22:30:47 +0100

    Fixed header from the POC + begin the front of the authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,11 +10,13 @@ import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material';
 import { TripComponent } from './trip/trip.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ActivityComponent } from './activity/activity.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,11 @@ import { ActivityComponent } from './activity/activity.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatMenuModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,52 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() coreToDisplay: EventEmitter<string> = new EventEmitter();
+    @Output() coreToDisplay: EventEmitter<string> = new EventEmitter()
+    closeResult: string
+    email: string
+    password: string
 
-  constructor() { }
+    constructor(private modalService: NgbModal) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  updateCore(target: string) {
-    this.coreToDisplay.emit(target)
-  }
+    updateCore(target: string) {
+        this.coreToDisplay.emit(target)
+    }
+
+    open(content) {
+        this.modalService.open(content).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            console.log(this.closeResult)
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+    onSubmit(form: NgForm) {
+        this.email = form.value.email
+        this.password = form.value.password
+        console.log(this.email);
+        console.log(this.password);
+      }
 
 }
